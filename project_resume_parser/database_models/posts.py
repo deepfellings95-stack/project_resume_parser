882f7b9864726d677f384cfae2235778845d7011
deepfellings95-stack <deepfellings95@gmail.com>
2025-11-28 15:47:54 +0530

    Delete project_resume_parser directory

--- project_resume_parser/database_models/posts.py
+++
@@ -1,8 +0,0 @@
-from database.database import db
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(400))
-    text = db.Column(db.Text)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
